a420f729dbc1c133fcf6fa8745871c1077275104
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-08-08 01:23:13 -0400

    S03-L39 Default Profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by dmitri on 2020-08-08.
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
